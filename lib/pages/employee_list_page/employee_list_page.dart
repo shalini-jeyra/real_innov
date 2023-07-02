15faf1d69cff6945d0ad359e08af16dbccb3649f
shalinicez <ashlin@air7seas.us>
2023-07-02 22:17:38 +0530

    Add previus/current employee

--- lib/pages/employee_list_page/employee_list_page.dart
+++ lib/pages/employee_list_page/employee_list_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
 import 'package:real_innov/pages/pages.dart';
@@ -6,7 +7,6 @@ import 'package:real_innov/styles/styles.dart';
 
 import '../../bloc/employee_bloc.dart';
 import '../../models/employee.dart';
-
 class EmployeeListPage extends StatefulWidget {
   const EmployeeListPage({Key? key}) : super(key: key);
 
@@ -16,7 +16,8 @@ class EmployeeListPage extends StatefulWidget {
 
 class _EmployeeListPageState extends State<EmployeeListPage> {
   late EmployeeBloc employeeBloc;
-List<Employee> employees = [];
+  List<Employee> previousEmployees = [];
+  List<Employee> currentEmployees = [];
 
   @override
   void initState() {
@@ -30,15 +31,29 @@ List<Employee> employees = [];
     super.dispose();
   }
 
- void _deleteEmployee(Employee employee) {
+  void _deleteEmployee(Employee employee) {
     employeeBloc.add(DeleteEmployeeEvent(employee));
   }
+void _updateEmployeesList(List<Employee> employees) {
+  final currentDate = DateTime.now();
+  final List<Employee> previous = [];
+  final List<Employee> current = [];
 
-  void _updateEmployeesList(List<Employee> updatedEmployees) {
-    setState(() {
-      employees = updatedEmployees;
-    });
+  for (final employee in employees) {
+    if (employee.endDate.isBefore(currentDate)) {
+      previous.add(employee);
+    } else {
+      current.add(employee);
+    }
   }
+
+  SchedulerBinding.instance!.addPostFrameCallback((_) {
+    setState(() {
+      previousEmployees = previous;
+      currentEmployees = current;
+    });
+  });
+}
 
 
   @override
@@ -55,10 +70,11 @@ List<Employee> employees = [];
         bloc: employeeBloc,
         builder: (context, state) {
           if (state is EmployeeLoadingState) {
-            return Center(child: CircularProgressIndicator());
+            return const Center(child: CircularProgressIndicator());
           } else if (state is EmployeeLoadedState) {
-               employees = List<Employee>.from(state.employees);
-   print(employees);
+            final List<Employee> employees = List<Employee>.from(state.employees);
+            _updateEmployeesList(employees);
+
             if (employees.isEmpty) {
               return Center(
                 child: Image.asset(
@@ -67,42 +83,13 @@ List<Employee> employees = [];
               );
             }
 
-            return ListView.builder(
-              itemCount: employees.length,
-              itemBuilder: (context, index) {
-                final employee = employees[index];
-                final startDateFormatted =
-                    DateFormat.yMMMd().format(employee.startDate);
-                final endDateFormatted =
-                    DateFormat.yMMMd().format(employee.endDate);
-
-                return Dismissible(
-                  key: UniqueKey(),
-                  direction: DismissDirection.endToStart,
-                  background: Container(
-                    color: Colors.red,
-                    alignment: Alignment.centerRight,
-                    padding: EdgeInsets.symmetric(horizontal: 16.0),
-                    child: Icon(Icons.delete, color: Colors.white),
-                  ),
-                  onDismissed: (direction) {
-                
-                  
-                        _deleteEmployee(employee);
-                          setState(() {
-                      employees.removeAt(index);
-                    });
-                   _updateEmployeesList(employees);
-                    print(employees);
-                  },
-                  child: ListTile(
-                    title: Text(employee.name),
-                    subtitle: Text(
-                      'Start Date: $startDateFormatted\nEnd Date: $endDateFormatted',
-                    ),
-                  ),
-                );
-              },
+            return ListView(
+              padding: const EdgeInsets.all(16.0),
+              children: [
+                _buildEmployeeList(previousEmployees, 'Previous Employees'),
+                const SizedBox(height: 16.0),
+                _buildEmployeeList(currentEmployees, 'Current Employees'),
+              ],
             );
           } else if (state is EmployeeErrorState) {
             return Center(child: Text(state.errorMessage));
@@ -129,4 +116,57 @@ List<Employee> employees = [];
       ),
     );
   }
+Widget _buildEmployeeList(List<Employee> employees, String heading) {
+  if (employees.isEmpty) {
+    return Container();
+  }
+
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Text(
+        heading,
+        style: const TextStyle(
+          fontSize: 18.0,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      const SizedBox(height: 8.0),
+      ListView.separated(
+        shrinkWrap: true,
+        itemCount: employees.length,
+        separatorBuilder: (context, index) => const Divider(), // Add a separator between items
+        itemBuilder: (context, index) {
+          final employee = employees[index];
+          final startDateFormatted = DateFormat.yMMMd().format(employee.startDate);
+          final endDateFormatted = DateFormat.yMMMd().format(employee.endDate);
+
+          return Dismissible(
+            key: Key(employee.id.toString()),
+            direction: DismissDirection.endToStart,
+            background: Container(
+              color: Colors.red,
+              alignment: Alignment.centerRight,
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: const Icon(Icons.delete, color: Colors.white),
+            ),
+            onDismissed: (direction) {
+              setState(() {
+                employees.removeAt(index);
+              });
+              _deleteEmployee(employee);
+            },
+            child: ListTile(
+              title: Text(employee.name),
+              subtitle: Text(
+                'Start Date: $startDateFormatted\nEnd Date: $endDateFormatted',
+              ),
+            ),
+          );
+        },
+      ),
+    ],
+  );
+}
+
 }
